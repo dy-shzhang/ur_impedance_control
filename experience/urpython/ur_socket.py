43c7d21a720cc9abb06e5c2a2da349eb7f2f6197
jetaimezsh <dyzsh1994@163.com>
2019-12-26 10:41:41 +0800

    新增experience文件夹,用来记录我做过的尝试

---
+++ experience/urpython/ur_socket.py
@@ -0,0 +1,26 @@
+import socket
+import time
+HOST ='192.168.1.50'
+PORT =30003
+ADDR=(HOST,PORT)
+robot = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+robot.connect(ADDR)
+
+
+
+def ur_movel(p,a=0.12,v=0.02,t=0,r=0):
+    str_command = "movel(p[%(x)s,%(y)s,%(z)s,%(rx)s,%(ry)s,%(rz)s],a=%(a)s,v=%(v)s,t=%(t)s,r=%(r)s) \n" %{'x':p[0],'y':p[1],'z':p[2],'rx':p[3],'ry':p[4],'rz':p[5],'a':a,'v':v,'t':t,'r':r}
+    robot.send(str_command.encode())
+
+p=[-0.11865,-0.46516,0.30444,0.0721,-3.1324,0.0551]
+
+
+print(p)
+while(True):
+    ur_movel(p)
+    print("order send")
+    time.sleep(0.008)
+    p2 = [0.11865,-0.46516,0.30444,0.0721,-3.1324,0.0551]
+    ur_movel(p2)
+    print("secondorder send")
+    time.sleep(0.008)
